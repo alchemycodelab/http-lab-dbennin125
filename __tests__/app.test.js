87d3de2db1535e1d75530b6bbe3bfbcf2f0edfcd
Daniel Bennington <dbennington125@gmail.com>
2020-06-08 15:29:50 -0700

    added notes for  parserequest

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,10 @@
 const request = require('supertest');
 const app = require('../lib/app');
-const createResponse = require('../lib/utils/createResponse.js');
+
 
 describe('createResponse', () => {
   it('get route', () => {
+    
     return request(app)
       .get('/')
       .then(res => {
@@ -11,6 +12,7 @@ describe('createResponse', () => {
       });
   });
   it('get red route', () => {
+    
     return request(app)
       .get('/red')
       .then(res => {
@@ -18,6 +20,7 @@ describe('createResponse', () => {
       });
   });
   it('get green route', () => {
+    
     return request(app)
       .get('/green')
       .then(res => {
@@ -25,21 +28,26 @@ describe('createResponse', () => {
       });
   });
   it('get blue route', () => {
+    
     return request(app)
       .get('/blue')
       .then(res => {
         expect(res.text).toEqual('<h1>blue</h1>');
       });
   });
-
   it('get echo route working', () => {
-    const requestedResponse = createResponse({ body: 'Yolo', status: '200 OK', contentType: 'text/plain' });
+    const requestedResponse = 
+    {        
+      body: 'Yolo',
+      status: '200 OK',
+      contentType: 'text/plain'
+    };
+    
     return request(app)
-      .get('/echo')
+      .post('/echo')
       .send(requestedResponse.body)
       .then(res => {
         expect(res.text).toEqual(requestedResponse.body);
       });
   });
-
 });
