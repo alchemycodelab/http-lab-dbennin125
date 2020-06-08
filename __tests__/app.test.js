c0289a551237a0e08f4c828744d5342875bee274
Daniel Bennington <dbennington125@gmail.com>
2020-06-08 14:49:52 -0700

    new parserequest and test  per lucia

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const createResponse = require('../lib/utils/createResponse.js');
 
 describe('createResponse', () => {
   it('get route', () => {
@@ -9,4 +10,36 @@ describe('createResponse', () => {
         expect(res.text).toEqual('hi');
       });
   });
+  it('get red route', () => {
+    return request(app)
+      .get('/red')
+      .then(res => {
+        expect(res.text).toEqual('<h1>red</h1>');
+      });
+  });
+  it('get green route', () => {
+    return request(app)
+      .get('/green')
+      .then(res => {
+        expect(res.text).toEqual('<h1>green</h1>');
+      });
+  });
+  it('get blue route', () => {
+    return request(app)
+      .get('/blue')
+      .then(res => {
+        expect(res.text).toEqual('<h1>blue</h1>');
+      });
+  });
+
+  it('get echo route working', () => {
+    const requestedResponse = createResponse({ body: 'Yolo', status: '200 OK', contentType: 'text/plain' });
+    return request(app)
+      .get('/echo')
+      .send(requestedResponse.body)
+      .then(res => {
+        expect(res.text).toEqual(requestedResponse.body);
+      });
+  });
+
 });
