7a381f13d5d6f6e571626325c5017301367efb56
Daniel Bennington <dbennington125@gmail.com>
2020-06-08 14:02:56 -0700

    get route hi works!

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,11 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
-
+  it('get route', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual('hi');
+      });
+  });
 });
